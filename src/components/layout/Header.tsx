634f3a6c908682a0d67d80360a70c73c31367da2
sijun <sionwer5@gmail.com>
2026-02-08 19:16:48 +0900

    feat: phone call 버튼 수정

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -55,7 +55,7 @@ export default function Header() {
               빠른 상담
             </button>
             <a
-              href="tel:0000-0000"
+              href="tel:1533-7117"
               className="px-4 py-2 h-10 text-base font-medium border border-primary text-primary rounded-lg hover:bg-primary hover:text-white transition-colors flex items-center gap-1"
             >
               <svg
@@ -67,7 +67,7 @@ export default function Header() {
               >
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
               </svg>
-              <span className="hidden md:inline">0000-0000</span>
+              <span className="hidden md:inline">1533-7117</span>
             </a>
           </nav>
         </div>
