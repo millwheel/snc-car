d2e4c7db911ba766b325fe11bc48ec150c58f5ed
sijun <sionwer5@gmail.com>
2026-02-11 13:24:43 +0900

    feat: 푸터 중앙 배치

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -2,37 +2,15 @@ export default function Footer() {
   return (
     <footer className="bg-[#2d3748] text-white py-10">
       <div className="container mx-auto px-4 max-w-4xl">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
-          {/* 회사 정보 */}
-          <div>
-            <h3 className="text-lg font-bold mb-4">S&C 신차장기렌트리스</h3>
-            <div className="space-y-2 text-sm text-white/70">
-              <p>상호명: 에쓰엔씨오토홀딩스 (대표: 정성우, 김용태)</p>
-              <p>사업자등록번호: 474-77-00620</p>
-              <p>주소: 인천광역시 남동구 호구포로 194, 1116호 (마크원 지식산업센터)</p>
-              <p>대표전화: 1866-1185</p>
-              <p>이메일: dydxo3029@naver.com, wjdtjddn37@naver.com</p>
-            </div>
+        <div className="text-center">
+          <h3 className="text-lg font-bold mb-4">S&C 신차장기렌트리스</h3>
+          <div className="space-y-2 text-sm text-white/70">
+            <p>상호명: 에쓰엔씨오토홀딩스 (대표: 정성우, 김용태)</p>
+            <p>사업자등록번호: 474-77-00620</p>
+            <p>주소: 인천광역시 남동구 호구포로 194, 1116호 (마크원 지식산업센터)</p>
+            <p>대표전화: 1866-1185</p>
+            <p>이메일: dydxo3029@naver.com, wjdtjddn37@naver.com</p>
           </div>
-
-          {/*/!* 링크 *!/*/}
-          {/*<div className="md:text-right">*/}
-          {/*  <h3 className="text-lg font-bold mb-4">고객 지원</h3>*/}
-          {/*  <div className="space-y-2 text-sm">*/}
-          {/*    <a*/}
-          {/*      href="#"*/}
-          {/*      className="block text-white/70 hover:text-white transition-colors"*/}
-          {/*    >*/}
-          {/*      개인정보처리방침*/}
-          {/*    </a>*/}
-          {/*    <a*/}
-          {/*      href="#"*/}
-          {/*      className="block text-white/70 hover:text-white transition-colors"*/}
-          {/*    >*/}
-          {/*      이용약관*/}
-          {/*    </a>*/}
-          {/*  </div>*/}
-          {/*</div>*/}
         </div>
 
         {/* 카피라이트 */}
